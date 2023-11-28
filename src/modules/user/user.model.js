f3925c7f79a0c30ef13e255942d21e091f34134c
Roya <foroughan.royaa@gmail.com>
2023-11-28 19:08:13 +0330

    UserModel

---
+++ src/modules/user/user.model.js
@@ -0,0 +1,15 @@
+const Schema = require('mongoose')
+const OTPSchema = new Schema({
+    code:{type: String , required:false , default:undefined},
+    expiresIn:{type:Number,require:false , default:0}
+})
+const UserSchema = new Schema({
+        fullname : {type : String ,  required:false},
+        mobile:{type : String , unique:true , require:true},
+        code:{OTPSchema},
+        verifiedMobile:{type:Boolean , default:false , required:true},
+
+},{timestamps : true})
+
+const UserModel = model('user' , UserSchema)
+module.exports = UserModel
