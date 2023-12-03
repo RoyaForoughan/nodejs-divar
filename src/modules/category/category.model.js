3452cdcd5f06047430152620d7345c83a992e37f
Roya <foroughan.royaa@gmail.com>
2023-12-03 12:00:48 +0330

    Category schema

---
+++ src/modules/category/category.model.js
@@ -0,0 +1,19 @@
+const { Schema, Types, model } = require("mongoose");
+
+const CategorySchema = new Schema({
+    name:{type:String , required:true},
+    slug:{type:String , require: true , index: true},
+    icon:{type:String , required:true},
+    parent:{type:Types.ObjectId, ref:'Category', required:false},
+    parents:{type:[Types.ObjectId] , ref:'Category', required:false , default:[]}
+},{virtuals:true , versionKey:false , id : false})
+
+CategorySchema.virtual('children',{
+    ref:'Category',
+    localField:'_id',
+    foreignField:'parent'
+})
+
+const CategoryModel = model('category' , CategorySchema)
+
+module.exports = CategoryModel
